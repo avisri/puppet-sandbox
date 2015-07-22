dab7e42dd334df720dbcc7c2e304fdbbfad43c3b
Sri Avi <sri@avi.com>
2015-07-22 18:00:48 +0000

    one more lazy file for committing

---
+++ cowardly_update_to_links.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dsts=( /etc/hiera.yaml 		/etc/puppet/hiera.yaml  /etc/puppet/modules /etc/puppet/puppet.conf /etc/puppet/hieradata )
+srcs=( /vagrant/hiera.yaml 	/vagrant/hiera.yaml 	/vagrant/modules    /vagrant/puppet.conf    /vagrant/hieradata )
+
+#ln -s /etc/hiera.yaml  /etc/puppet/hiera.yaml
+#ln -s /vagrant/hiera.yaml  /etc/hiera.yaml
+#ln -s /vagrant/modules
+#ln -s /vagrant/puppet.conf
+#ln -s /vagrant/hieradata
+
+count=0
+for dst in ${dsts[*]}
+do 
+  src="${srcs[$count]}"
+  ((count++))
+  echo "----------------[$count/${#dsts[*]}]-----------"
+  echo "$dst will be linked to $src -----"
+  echo mv -v $dst $dst.orig.`date +%s`
+  echo ln -v -s $src $dst 
+done
