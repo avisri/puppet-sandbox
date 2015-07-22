b3939d8ffa4da54e10da5b5b6daf089bd07e037f
Avinash Sridhar <avinash.sridhar@salesforce.com>
2015-07-22 10:17:48 -0700

    adding kibana

---
+++ validate_internal_network_route.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+echo 'for some reason my virtualbox would not add this route 
+on its own and i had to add it manually  
+$ifconfig vboxnet0
+vboxnet0: flags=8943<UP,BROADCAST,RUNNING,PROMISC,SIMPLEX,MULTICAST> mtu 1500
+	ether 0a:00:27:00:00:00
+	inet 172.16.32.1 netmask 0xffffff00 broadcast 172.16.32.255
+
+After adding the route it showed this:
+$ netstat -nr | grep 172.16.32
+172.16.32/24       link#13            UCSc            3        0 vboxnet
+'
+echo
+echo
+echo 'Your result ---- '
+netstat -nr | grep  172.16.32 ||  ( sudo route -nv add -net '172.16.32'  -interface vboxnet0  && etstat -nr | grep  172.16.32 ) 
